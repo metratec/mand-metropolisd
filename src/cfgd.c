bc922a1ea3b806c5c5c84207452de3066f947a09
Explorer001 <lukas.gehreke@gmail.com>
2021-05-18 13:30:25 +0200

    Encoding username to subscribed in topics when writing mosquitto.conf.

--- src/cfgd.c
+++ src/cfgd.c
@@ -474,14 +474,13 @@ void set_mosquitto(const char *host, uint16_t port,
 	        "topic spBv1.0/+/DDATA/# out 1\n"
 	        "topic spBv1.0/+/NDEATH/# out 1\n"
 	        "topic spBv1.0/+/DDEATH/# out 1\n"
-	        "topic spBv1.0/+/NCMD/# in 2\n"
-	        "topic spBv1.0/+/DCMD/# in 2\n"
+	        "topic spBv1.0/+/+/%s/# in 2\n"
 	        /*
 	         * We assume that we can only receive STATE messages from the backend server
 	         * we're bridging to.
 	         */
 	        "topic STATE/# in 1\n",
-	        PACKAGE_STRING, host, port);
+	        PACKAGE_STRING, host, port, username);
 
 	if (username && *username && password && *password)
 		fprintf(fout, "remote_clientid %s\n"
